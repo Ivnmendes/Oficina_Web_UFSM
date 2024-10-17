ebe077622c350d25dcc239fd0b58d7e01cba390c
Ivan <ivanmendes2005@gmail.com>
2024-10-17 19:06:33 -0300

    .

--- exercicio_framework/src/app/page.js
+++ exercicio_framework/src/app/page.js
@@ -5,6 +5,9 @@ export default function Home() {
   return (
     <div className={styles.mainDiv}>
       <main className={styles.main}>
+        <div className={styles.sideText}>
+          <p><strong>Calculadora muito foda</strong></p>
+        </div>
         <Calculator />
       </main>
       <footer className={styles.footer}>
